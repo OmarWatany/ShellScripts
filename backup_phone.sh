3b0793e8d90a35a7560fbe4ef44f25c2897a35e2
Omar Watany <watany6500@gmail.com>
2026-06-23 17:50:35 +0300

    update backup_phone

--- backup_phone.sh
+++ backup_phone.sh
@@ -6,26 +6,35 @@
 DST_PREFIX="$HOME/Phone"
 SRC_PREFIX="$HOME/fuse_mount/mobile"
 
-FOLDERS=("backups" "SwiftBackup" "Manga" "Download" "WhatsApp")
-FOLDERS_SRC_PREFIX=("" "" "" "" "Android/media/com.whatsapp/")
 
+# FOLDERS=("backups" "SwiftBackup" "Download" "Manga" "Videos" "WhatsApp")
+# FOLDERS_SRC_PREFIX=("" "" "" "" "" "Android/media/com.whatsapp/")
+
+FOLDERS=("SwiftBackup" "Download" "Manga" "Videos")
+FOLDERS_SRC_PREFIX=("" "" "" "")
 for i in ${!FOLDERS[@]}; do
-	DST="$DST_PREFIX/${FOLDERS[$i]}/"
-	SRC="$SRC_PREFIX/${FOLDERS_SRC_PREFIX[$i]}${FOLDERS[$i]}/"
+	DST="$DST_PREFIX/${FOLDERS[$i]}"
+	SRC="$SRC_PREFIX/${FOLDERS_SRC_PREFIX[$i]}${FOLDERS[$i]}/" #rsync
+
 	case $1 in
 	"dry")
+    echo -e "\e[32m|>\e[0m SRC: $SRC, DST: $DST"
 		rsync -arhv --dry-run --delete $SRC $DST
 		;;
 	"run")
-		rsync -arhv --delete $SRC $DST
+    echo -e "\e[32m|>\e[0m SRC: $SRC, DST: $DST"
+		rsync -arhvP --delete $SRC $DST
 		;;
 	"phone_dry")
-		rsync -arhv --dry-run --delete $DST $SRC
+    echo -e "\e[32m|>\e[0m SRC: $DST, DST: $SRC"
+		rsync -arhv --dry-run --delete "$DST/" $SRC
 		;;
 	"phone_run")
-		rsync -arhv --delete $DST $SRC
+    echo -e "\e[32m|>\e[0m SRC: $DST, DST: $SRC"
+		rsync -arhvP --delete "$DST/" $SRC
 		;;
 	*)
+		echo "Command Not Valid $1"
 		# rsync -arhv $@ $SRC $DST
 		;;
 	esac
